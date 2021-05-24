a424e33c65e2d5ebda8d62d2c53ab7f2d2cd6189
shuqin <shuqin@cmsr.chinamobile.com>
2021-05-24 18:32:50 +0800

    公司，多线程

---
+++ MultiThread/src/PrintABCUsingLock.java
@@ -0,0 +1,45 @@
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class PrintABCUsingLock {
+
+    private int times; // 控制打印次数
+    private int state;   // 当前状态值：保证三个线程之间交替打印
+    private Lock lock = new ReentrantLock();
+
+    public PrintABCUsingLock(int times) {
+        this.times = times;
+    }
+
+    private void printLetter(String name, int targetNum) {
+        for (int i = 0; i < times; ) {
+            System.out.println("i="+i+"   curThread:"+Thread.currentThread().getName());
+            lock.lock();
+            System.out.println(lock);
+            if (state % 3 == targetNum) {
+                state++;
+                i++;
+                System.out.println("aaaaaaaaa"+name+"   i="+i+"    state="+ (state - 1));
+
+            }
+            System.out.println("end");
+            lock.unlock();
+        }
+    }
+
+    public static void main(String[] args) {
+        PrintABCUsingLock loopThread = new PrintABCUsingLock(5);
+
+        new Thread(() -> {
+            loopThread.printLetter("B", 1);
+        }, "B").start();
+
+        new Thread(() -> {
+            loopThread.printLetter("A", 0);
+        }, "A").start();
+
+        new Thread(() -> {
+            loopThread.printLetter("C", 2);
+        }, "C").start();
+    }
+}
